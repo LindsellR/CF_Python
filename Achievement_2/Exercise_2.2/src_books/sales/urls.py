7bd660a7e45842af016d5fbd7f2589d2a4716f89
LindsellR <lindsellrichard@gmail.com>
2025-09-16 16:52:50 +1000

    Added task 2.7 folder and screenshots

--- Achievement_2/Exercise_2.2/src_books/sales/urls.py
+++ Achievement_2/Exercise_2.2/src_books/sales/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, records
 
 # specify app name
 app_name = 'sales'
 
 # specify url path
 urlpatterns = [
-   path('', home),
+   path('', home, name='home'),
+   path('records/', records, name='records')
 ]
